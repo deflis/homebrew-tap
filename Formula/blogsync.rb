fd2b787011dbb1696e144e37cd5dba0abb81be46
deflis <deflis@gmail.com>
2023-01-07 15:18:13 +0900

    support ARM and Linux on blogsnyc

--- Formula/blogsync.rb
+++ Formula/blogsync.rb
@@ -5,6 +5,18 @@ class Blogsync < Formula
   sha256 '0f714449f1eeaf982c4f1802f2204e6b622bee3e0d3b45ea5370c01062d1d7e5'
   head 'https://github.com/motemen/blogsync.git'
 
+
+  on_macos do
+    on_arm do
+        url "https://github.com/x-motemen/blogsync/releases/download/v0.12.3/blogsync_v0.12.3_darwin_arm64.zip"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/x-motemen/blogsync/releases/download/v0.12.3/blogsync_v0.12.3_linux_amd64.tar.gz"
+  end
+
+
   def install
     bin.install 'blogsync'
   end
